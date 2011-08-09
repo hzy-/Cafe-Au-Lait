5f6de109f833bec9d97da7e213f79a4366b83ada
Jacob Haslehurst <jacob@haslehurst.net>
2011-08-09 19:31:43 +0800

    new order page mainly designed, now onto the functionality...

--- html/style/scripts.js
+++ html/style/scripts.js
@@ -1,11 +1,35 @@
+//executes all this after the DOM has loaded
 $(document).ready(function(){
 	//on load hides all divs except the home one
-	$("div").slice(2).hide();
+	$('div').slice(2).hide();
 
-	//switches page to #new_order
-	$('#new_order').click(function(){
+	//front page menu
+	$('#buttons li a').click(function(){
+		var action = $(this).attr("href").replace("#", "");
 		$("#p_home").fadeOut(200, function(){
-			$("#p_new_order").fadeIn(200);
+			$("#p_"+action).fadeIn(200);
 		});
 	});
+
+	//header link
+	$('h1 a').click(function(){
+		$('div#wrap>div').fadeOut(200, function(){
+			$("#p_home").fadeIn(200);
+		});
+	});
+
+	//New Order Page
+	//hides takeaway by default and sets 
+	$('ul#takeaway').hide();
+	$('#t_dinein').css('border-bottom', '10px solid #036902');
+	//switches between which menu is active
+	$('ul#dinein_takeaway a').click(function(){
+		var action = $(this).attr('id').replace('t_', '');
+		$('#dinein, #takeaway').fadeOut(200, function(){
+			$('#t_dinein, #t_takeaway').css('border', 'none');
+			$('#t_'+action).css('border-bottom', '10px solid #036902');
+			$('#'+action).fadeIn(200);
+		});
+	});
+
 });
